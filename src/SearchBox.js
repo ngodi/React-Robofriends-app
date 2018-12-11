83bac3940115f6f46c8102ff4b37666ef1803bca
ngodi <albertngodi@gmail.com>
2018-12-11 07:31:53 +0100

    searching implemented

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
     return (
       <div className="pa2">
         <input 
-           className='pa3 ba--green bg-lightest-blue'
+           className='pa3 b--green bg-lightest-blue'
            type='search' 
            placeholder='search robots' 
+           onChange = {searchChange}
         />
         </div>
     	);
